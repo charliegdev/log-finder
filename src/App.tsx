1b2635a7fb06ee3a74f6d235c4822993895df896
Charlie Guan <charliegdev@gmail.com>
2024-04-07 07:27:14 -0700

    Add app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
 import styles from "./App.module.scss";
 
 function App() {
   return (
     <div className={styles.app}>
-      <div>Warcraft Log Finder</div>
-      <div>Paste the Raider.IO profile URL below</div>
+      <AppBar classes={{ root: styles.appbar }} position="static">
+        <Toolbar classes={{ root: styles.toolbar }}>
+          <Typography classes={{ root: styles.title }} variant="h6">
+            Warcraft Log Finder
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div className={styles.container}>
+        <div>Paste the Raider.IO profile URL below</div>
+      </div>
     </div>
   );
 }
